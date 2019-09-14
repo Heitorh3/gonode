f8de9ec32977a89a6cda9e3860fe55f46fd3abea
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2019-09-14 18:42:18 -0300

    Criando rota para file e agrupando as rotas autenticadas

--- start/routes.js
+++ start/routes.js
@@ -9,4 +9,7 @@ Route.post("sessions", "SessionController.store");
 Route.post("forgot", "ForgotPasswordController.store");
 Route.post("reset", "ResetPasswordController.store");
 
+Route.post("files/:id", "FileController.show");
+
+Route.group(() => {
 Route.post("files", "FileController.store");
