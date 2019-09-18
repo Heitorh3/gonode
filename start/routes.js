80d6af73568015efbd33e4947a568511b2054ee7
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2019-09-18 14:52:17 -0300

    resource/task router

--- start/routes.js
+++ start/routes.js
@@ -1,18 +1,19 @@
-"use strict";
+
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use("Route");
+const Route = use('Route');
 
-Route.post("users", "UserController.store").validator("User");
-Route.post("sessions", "SessionController.store").validator("Session");
+Route.post('users', 'UserController.store').validator('User');
+Route.post('sessions', 'SessionController.store').validator('Session');
 
-Route.post("forgot", "ForgotPasswordController.store").validator("Forgot");
-Route.post("reset", "ResetPasswordController.store").validator("Reset");
+Route.post('forgot', 'ForgotPasswordController.store').validator('Forgot');
+Route.post('reset', 'ResetPasswordController.store').validator('Reset');
 
-Route.get("files/:id", "FileController.show");
+Route.get('files/:id', 'FileController.show');
 
 Route.group(() => {
-  Route.post("files", "FileController.store");
+  Route.post('files', 'FileController.store');
 
-  Route.resource("projects", "ProjectController").apiOnly();
-}).middleware(["auth"]);
+  Route.resource('projects', 'ProjectController').apiOnly();
+  Route.resource('projects.tasks', 'TaskController').apiOnly();
+}).middleware(['auth']);
