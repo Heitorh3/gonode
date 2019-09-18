fc3972827aa3a5921e296fa4577e2d3b509ec6d5
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2019-09-18 18:03:13 -0300

    feature/internacionalizacao

--- app/Validators/Reset.js
+++ app/Validators/Reset.js
@@ -1,3 +1,4 @@
+const Antl = use('Antl');
 class Reset {
   get validateAll() {
     return true;
@@ -8,6 +9,10 @@ class Reset {
       token: 'required',
       password: 'required|confirmed',
     };
+  }
+
+  get messages() {
+    return Antl.list('validation');
   }
 }
 
